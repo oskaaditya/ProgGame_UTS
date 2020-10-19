175a6d33e9051565f1136b24a94100e8c962cf92
oskaaditya <oska.aditya18@gmail.com>
2020-10-19 08:58:49 +0700

    Ujian Tengah Semester Pemrograman Game

---
+++ Assets/Scripts/HalamanUtama.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HalamanUtama : MonoBehaviour
+{
+    public void PindahHalaman(string halaman)
+    {
+        SceneManager.LoadScene(halaman);
+    }
+
+    public void KeluarAplikasi()
+    {
+        Application.Quit();
+    }
+}
